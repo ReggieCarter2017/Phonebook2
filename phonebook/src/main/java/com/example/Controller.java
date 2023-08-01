d81e9ff5eb26975c61d2401fa0d16d827e1bbe7e
ReggieCarter <coolpagani@gmail.com>
2023-08-01 13:04:37 +0300

    Commit

--- phonebook/src/main/java/com/example/Controller.java
+++ phonebook/src/main/java/com/example/Controller.java
@@ -1,27 +1,32 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Controller {
-    public void runProgram()
-    {
+
+    ArrayList<Person> persons = new ArrayList<>();
+
+    public void runProgram() {
         View vi = new View();
         Phone p = new Phone();
 
         boolean temp = true;
 
-        while (temp)
-        {
+        while (temp) {
             String name = vi.input("Enter name: ");
             String surname = vi.input("Enter surname: ");
-            String phonenumber = vi.input("Enter phone number: ");
-            String dateOfBirth = p.validateNumbers(addPhone());
+            String phoneNumber = p.validateNumbers(addPhone("Enter phone number: "));
+            String dateOfBirth = vi.input("Enter date of birth: ");
             String sex = vi.input("Enter the gender of the person: ");
+
+            addPerson(new Person(name, surname, phoneNumber, dateOfBirth, sex));
+            showPerson();
         }
     }
 
-    public String addPhone()
-    {
+    public String addPhone(String string) {
+        System.out.println(string);
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please, enter your phone number using only numbers: ");
@@ -30,9 +35,22 @@ public class Controller {
 
         return tempNumber;
 
-        
+    }
+
+    public void addPerson(Person p)
+    {
+        persons.add(p);
     }
 
 
+    public void showPerson()
+    {
+        for (Person e : persons)
+        System.out.println("Name: " + e.getName()
+        + "\nSurname: " + e.getSurname() +
+        "\nPhone number: " + e.getPhoneNumber() +
+        "\nDate of birth: " + e.getDateOfBirth()
+        + "\nGender: " + e.getSex());
+    }
 
 }
--- phonebook/src/main/java/com/example/Main.java
+++ phonebook/src/main/java/com/example/Main.java
@@ -2,13 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Phone p = new Phone();
         Controller c = new Controller();
 
-
-        String d = p.validateNumbers(c.addPhone());
-
-        System.out.println(d);
-
+        c.runProgram();
     }
 }
