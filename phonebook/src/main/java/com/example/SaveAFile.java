519ff25d62158b587917fb3d164ec24197caf522
ReggieCarter <coolpagani@gmail.com>
2023-08-02 13:43:18 +0300

    Commit 9

--- phonebook/src/main/java/com/example/SaveAFile.java
+++ phonebook/src/main/java/com/example/SaveAFile.java
@@ -8,12 +8,12 @@ public class SaveAFile
 {
     public void saveTxt(Person e)
     {
-        String data = "Name: " + e.getName() + ". Surname: " + e.getSurname() + ". Father name: " + e.getFatherName()
-        + e.getPhoneNumber() + ". Date of birth: " + e.getDateOfBirth() + ". Gender: " + e.getGender();
+        String data = "Name: " + e.getName() + ". Surname: " + e.getSurname() + ". Father name: " + e.getFatherName() + ". Phone number: " + 
+        e.getPhoneNumber() + ". Date of birth: " + e.getDateOfBirth() + ". Gender: " + e.getGender() + ".\n";
         String name = e.getSurname() + ".txt";
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(name));
-            writer.write(data);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(name, true));
+            writer.append(data);
             writer.close();
         } catch (IOException e1) {
             e1.printStackTrace();
